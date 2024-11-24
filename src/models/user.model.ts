4c00e901b2a5aa861da9a6137e100844f7d123d5
easily-mistaken <choubeyprajjwal@gmail.com>
2024-11-25 00:00:44 +0530

    hook added

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import bcrypt from "bcrypt";
 
 const userSchema = new mongoose.Schema(
   {
@@ -46,5 +47,10 @@ const userSchema = new mongoose.Schema(
   },
   { timestamps: true }
 );
+
+userSchema.pre("save", async function (next) {
+  this.password = bcrypt.hash(this.password, 10);
+  next();
+});
 
 export const User = mongoose.model("User", userSchema);
